e568c903a8327b0d205191485b21b278f20d478c
Elena Ermolenkova <e-wolf@rambler.ru>
2025-04-09 00:04:55 +0300

    Созданы тесты для 1 метода

--- tests.py
+++ tests.py
@@ -1,6 +1,6 @@
 import pytest
 class TestBooksCollector:
-
+# Тесты на первый метод
     def test_add_new_book_add_two_books(self, collector):
         collector.add_new_book('Гордость и предубеждение и зомби')
         collector.add_new_book('Что делать, если ваш кот хочет вас убить')
