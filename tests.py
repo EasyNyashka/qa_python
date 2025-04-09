ae757a1dfde2d956897a7f8017b50ec2ed867dfa
Elena Ermolenkova <e-wolf@rambler.ru>
2025-04-09 19:41:08 +0300

    Добавила два позитивных теста для 1 метода

--- tests.py
+++ tests.py
@@ -1,12 +1,13 @@
 import pytest
 class TestBooksCollector:
 # Тесты на первый метод
-    def test_add_new_book_add_two_books(self, collector):
-        collector.add_new_book('Гордость и предубеждение и зомби')
-        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
-        assert len(collector.books_genre) == 2
+    @pytest.mark.parametrize('book_name', ['Я','Гордость и предубеждение и зомби', 'В названии книги ровно сорок символов 40'])
+    def test_add_new_book_add_one_books(self, collector, book_name):
+        collector.add_new_book(book_name)
+        #collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+        assert len(collector.books_genre) == 1
 
-    @pytest.mark.parametrize('book_name', ['Очень длинное название книги, очень-очень-очень', ''])
+    @pytest.mark.parametrize('book_name', ['В названии книги ровно сорок один символ ', 'Очень длинное название книги, очень-очень-очень', ''])
     def test_add_new_book_invalid_name_book(self, collector, book_name):
         collector.add_new_book(book_name)
         assert len(collector.books_genre) == 0
