2869fd6e056954c34eb151bb0450bcf0677a542e
Elena Ermolenkova <e-wolf@rambler.ru>
2025-04-10 21:18:22 +0300

    сохранила README.md написала тесты 3 метод

--- tests.py
+++ tests.py
@@ -1,10 +1,9 @@
 import pytest
 class TestBooksCollector:
-# Тесты на первый метод
+# Tесты на первый метод
     @pytest.mark.parametrize('book_name', ['Я','Гордость и предубеждение и зомби', 'В названии книги ровно сорок символов 40'])
     def test_add_new_book_add_one_books(self, collector, book_name):
         collector.add_new_book(book_name)
-        #collector.add_new_book('Что делать, если ваш кот хочет вас убить')
         assert len(collector.books_genre) == 1
 
     @pytest.mark.parametrize('book_name', ['В названии книги ровно сорок один символ ', 'Очень длинное название книги, очень-очень-очень', ''])
@@ -31,5 +30,19 @@ class TestBooksCollector:
         collector.add_new_book('Домовенок Кузя')
         collector.set_book_genre('Домовенок Кузя', 'Советские мультфильмы')
         assert collector.get_book_genre('Домовенок Кузя') == ''
+
+        # Тесты на 3 метод
+    def test_get_book_genre_book_exists(self, collector):
+        collector.add_new_book('Домовенок Кузя')
+        collector.set_book_genre('Домовенок Кузя', 'Мультфильмы')
+        assert collector.get_book_genre('Домовенок Кузя') == 'Мультфильмы'
+
+    def test_get_book_genre_invalid_genre_book(self, collector):
+        collector.add_new_book('Домовенок Кузя')
+        collector.set_book_genre('Домовенок Кузя', 'Мультфильмы')
+        assert collector.get_book_genre('Домовенок Кузя') != 'Детективы'
+
+    def test_get_book_genre_book_nonexists(self, collector):
+        assert collector.get_book_genre('Домовенок Кузя') is None
 
 
