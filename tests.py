7dbea785419a7edfe6aee4d04265f84bdc13bb1d
Elena Ermolenkova <e-wolf@rambler.ru>
2025-04-11 20:43:51 +0300

    Тесты к 6 методу и прокомментировала результат в README.md

--- tests.py
+++ tests.py
@@ -2,7 +2,7 @@ import pytest
 class TestBooksCollector:
 # Tесты на первый метод
     @pytest.mark.parametrize('book_name', ['Я','Домовенок Кузя', 'В названии книги ровно сорок символов 40'])
-    def test_add_new_book_add_one_books(self, collector, book_name):
+    def test_add_new_book_add_one_book(self, collector, book_name):
         collector.add_new_book(book_name)
         assert len(collector.books_genre) == 1
 
@@ -31,7 +31,7 @@ class TestBooksCollector:
         collector.set_book_genre('Домовенок Кузя', 'Советские мультфильмы')
         assert collector.get_book_genre('Домовенок Кузя') == ''
 
-        # Тесты на 3 метод
+# Тесты на 3 метод
     def test_get_book_genre_book_exists(self, collector):
         collector.add_new_book('Домовенок Кузя')
         collector.set_book_genre('Домовенок Кузя', 'Мультфильмы')
@@ -45,7 +45,7 @@ class TestBooksCollector:
     def test_get_book_genre_book_nonexists(self, collector):
         assert collector.get_book_genre('Домовенок Кузя') is None
 
-    # Тесты на 4 метод
+# Тесты на 4 метод
     def test_get_books_with_specific_genre_valid_gerne(self, collector):
         collector.add_new_book('Домовенок Кузя')
         collector.add_new_book('Золушка')
@@ -59,13 +59,32 @@ class TestBooksCollector:
     def test_get_books_with_specific_genre_invalid_gerne(self, collector):
         assert collector.get_books_with_specific_genre('Советские мультфильмы') == []
 
-   # Тесты на 5 метод
-    def test_get_books_genre_not_an_empty_dict(self, collector):
+# Тесты на 5 метод
+    def test_get_books_genre_not_an_empty_dictionary(self, collector):
         collector.add_new_book('Золушка')
         collector.add_new_book('Оно')
         collector.set_book_genre('Золушка', 'Мультфильмы')
         collector.set_book_genre('Оно', '')
         assert collector.get_books_genre() == {'Золушка': 'Мультфильмы', 'Оно':''}
 
-    def test_get_books_genre_empty_dict(self, collector):
+    def test_get_books_genre_empty_dictionary(self, collector):
         assert collector.get_books_genre() == {}
+
+# Тесты на 6 метод
+    def test_get_books_for_children_no_adult_books(self, collector):
+        collector.add_new_book('Затерянный мир')
+        collector.add_new_book('Домовенок Кузя')
+        collector.set_book_genre('Затерянный мир', 'Фантастика')
+        collector.set_book_genre('Домовенок Кузя', 'Мультфильмы')
+        assert collector.get_books_for_children() == ['Затерянный мир', 'Домовенок Кузя']
+
+    def test_get_books_for_children_with_adult_books(self, collector):
+        collector.add_new_book('Оно')
+        collector.add_new_book('Домовенок Кузя')
+        collector.set_book_genre('Оно', 'Ужасы')
+        collector.set_book_genre('Азазель', 'Детектив')
+        assert collector.get_books_for_children() == []
+
+
+
+
